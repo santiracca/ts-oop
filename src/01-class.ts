527abf077438c19c17bb10dec5ab828cc0391e88
Santi Racca <santiagoracca7@gmail.com>
2022-10-12 10:32:28 -0300

    add base class, add methods

---
+++ src/01-class.ts
@@ -0,0 +1,14 @@
+export class MyDate {
+  year: number;
+  month: number;
+  day: number;
+
+  constructor(year: number, month: number, day: number) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+}
+
+const myDate = new MyDate(2022, 1, 13);
+console.log(myDate);
