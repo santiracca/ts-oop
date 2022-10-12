b059f260d55bfc787f35f96846316237d643aabb
Santi Racca <santiagoracca7@gmail.com>
2022-10-12 14:34:39 -0300

    create product memory service

---
+++ src/app/dtos/product.dto.ts
@@ -0,0 +1,8 @@
+import { Category } from '../models/category.model';
+import { Product } from '../models/product.model';
+
+export interface CreateProductDto extends Omit<Product, 'id' | 'category'> {
+  categoryId: Category['id'];
+}
+
+export interface UpdateProductDto extends Partial<CreateProductDto> {}
